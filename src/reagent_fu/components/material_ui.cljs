86db03ec2b582f8744dc562cc4c6149530f09f27
Saager Mhatre <saager.mhatre@gmail.com>
2020-11-02 16:12:01 -0500

    Format date ticks better

--- src/reagent_fu/components/material_ui.cljs
+++ src/reagent_fu/components/material_ui.cljs
@@ -1,5 +1,6 @@
 (ns reagent-fu.components.material-ui
   (:require
+    [clojure.string :as s]
     [reagent.core :as r]
     [goog.object :as gobj]
     ["@material-ui/core" :as mui]
@@ -21,8 +22,13 @@
 (def with-custom-styles (withStyles custom-styles))
 
 (defn day [timestamp]
-  (let [d (js/Date. (* timestamp 1000))]
-    (str (.getDate d) "/" (inc (.getMonth d)))))
+  (-> (js/Date. (* timestamp 1000))
+      (.toDateString)
+      (s/split #" ")
+      (->> (rest)
+           (take 2)
+           (reverse)
+           (s/join " "))))
 
 (defn stock-data [data]
   (let [result (get-in data [:chart :result 0])
